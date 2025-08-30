febe07132a830bca32fd38be73e48ff007036be2
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2025-08-30 06:26:19 +0600

    hello world express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+
+const port =8080;
+
+
+app.get('/',(req,res) => {
+
+
+    res.send('hello with express');
+
+});
+
+
+app.listen(port, () =>{
+ console.log(`Example app listening at http://localhost:${port}`);
+  
+});
